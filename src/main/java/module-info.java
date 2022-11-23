99679372814b68240493cffb47483459683f0c0d
Haok224 <zhanglinghao2020@outlook.com>
2022-11-23 16:18:59 +0800

    文件拖拽实现

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.haok.resourcespackmader {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens org.haok.resourcespackmader to javafx.fxml;
     exports org.haok.resourcespackmader;
 }
---
+++ src/main/java/org/haok/resourcespackmader/AppController.java
@@ -0,0 +1,22 @@
+package org.haok.resourcespackmader;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+
+public class AppController {
+
+    @FXML
+    private TextField ttf_path;
+
+    @FXML
+    void ttf_dropped(DragEvent event) {
+
+    }
+    @FXML
+    void ttf_over(DragEvent event) {
+
+    }
+
+}
